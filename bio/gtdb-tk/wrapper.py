75e2259457e2d86ba350da82c70a6cfd8882ffa0
Rodolfo Brandão Dias Ferreira <rodolfobrandao88@gmail.com>
2026-05-01 13:16:56 +0100

    add

---
+++ bio/gtdb-tk/wrapper.py
@@ -0,0 +1,31 @@
+"""Snakemake wrapper for GTDB-Tk classify_wf."""
+
+__author__ = "Rodolfo Brandão Dias Ferreira"
+__copyright__ = "Copyright 2026, Rodolfo"
+__email__ = "arafaelogomes@gmail.com" 
+__license__ = "MIT"
+
+import os
+from snakemake.shell import shell
+
+extra = snakemake.params.get("extra", "")
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+db_path = snakemake.params.get("db_path", "")
+if db_path:
+    os.environ["GTDBTK_DATA_PATH"] = db_path
+
+out_dir = os.path.dirname(snakemake.output[0])
+
+extension = snakemake.params.get("extension", "fasta")
+
+shell(
+    "gtdbtk classify_wf"
+    " --genome_dir {snakemake.input[0]:q}"
+    " --out_dir {out_dir:q}"
+    " --extension {extension}"
+    " --cpus {snakemake.threads}"
+    " --force"
+    " {extra}"
+    " {log}"
+)
