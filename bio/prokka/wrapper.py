de58fc7c6c337c5570111d0c23e3e85fd614aba1
Rodolfo Brandão Dias Ferreira <rodolfobrandao88@gmail.com>
2026-05-12 16:09:59 +0100

    Update wrapper.py

--- bio/prokka/wrapper.py
+++ bio/prokka/wrapper.py
@@ -1,30 +1,43 @@
 """Snakemake wrapper for Prokka."""
 
-__author__ = "Rodolfo Brandão" 
-__copyright__ = "Copyright 2026, Rodolfo"
-__email__ = "rodolfobrandao88@gmail.com"
+__author__ = "Rodolfo Brandão Dias Ferreira"
+__copyright__ = "Copyright 2026, Rodolfo Brandão"
+__email__ = "rodolfobrandao88@gmail.com" 
 __license__ = "MIT"
 
 from pathlib import Path
 from snakemake.shell import shell
 
-extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+extra = snakemake.params.get("extra", "")
+
+
+proteins = snakemake.input.get("proteins", "")
+proteins_cmd = f"--proteins {proteins}" if proteins else ""
+
+kingdom = snakemake.params.get("kingdom", "")
+kingdom_cmd = f"--kingdom {kingdom}" if kingdom else ""
+
+genus = snakemake.params.get("genus", "")
+genus_cmd = f"--genus {genus}" if genus else ""
+
+species = snakemake.params.get("species", "")
+species_cmd = f"--species {species}" if species else ""
 
 first_output = Path(snakemake.output[0])
 outdir = first_output.parent
 prefix = first_output.stem
 
-proteins = snakemake.input.get("proteins", "")
-proteins_cmd = f"--proteins {proteins}" if proteins else ""
-
 shell(
     "prokka "
-    "{extra} "
     "--outdir {outdir} "
     "--prefix {prefix} "
     "--force "
     "{proteins_cmd} "
+    "{kingdom_cmd} "
+    "{genus_cmd} "
+    "{species_cmd} "
+    "{extra} "
     "{snakemake.input.fasta} "
     "{log}"
 )
